ee80f41a66bd09b1a778df73d823aa812d802a6d
AlexV <asmodehn@gmail.com>
2015-06-02 22:15:34 +0900

    temp commit of refactoring of flask views

--- rostful/src/rostful/__init__.py
+++ rostful/src/rostful/__init__.py
@@ -1,3 +1,7 @@
+### This package contains a Server ( instanciated once here to allow easy setup procedure, but not started )
+
+from . import server
+from .server import rostful_server
 
 
-
+### TODO This package also contains a Client
