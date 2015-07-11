7b1b34e980507e86c47b2bf8fbce436b7a50041a
AlexV <asmodehn@gmail.com>
2015-07-11 22:35:41 +0900

    improving rostful setup.py for working both with ros or without. still WIP.

--- rostful/setup.py
+++ rostful/setup.py
@@ -1,33 +1,54 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+# This setup is usable by catkin, or on its own as usual python setup.py
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+_CATKIN = False
+try:
+    from distutils.core import setup
+    from catkin_pkg.python_setup import generate_distutils_setup
+    _CATKIN = True
+except Exception, e:
+    from setuptools import setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=[
-        'rostful',
-    ],
-    install_requires=[
-        'rostful_node',
-        'Flask',
-        # TODO : list dependencies here to download them and avoid using flask_ext_catkin ??
-    ],
-    # not usable with catkin distutils version.
-    # Should work with setuptools.setup. Will include files from Manifest.in
-    # include_package_data=True,
-    package_data={
-        'rostful': [
-            'static/js/jquery/*',
-            'static/js/jquery-mobile/*',
-            'static/js/jquery-mobile/images/*',
-            'static/js/jquery-mobile/images/icons-png/*',
-            'static/js/jquery-mobile/images/icons-svg/*',
-            'templates/*',
-            'templates/security/*',
-            'templates/security/email/*',
+# CAREFUL distutils and setuptools take different argument sand have different behaviors
+if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
+    # fetch values from package.xml
+    setup_args = generate_distutils_setup(
+        packages=[
+            'rostful',
         ],
-    },
-)
+        package_data={
+            'rostful': [
+                'static/js/jquery/*',
+                'static/js/jquery-mobile/*',
+                'static/js/jquery-mobile/images/*',
+                'static/js/jquery-mobile/images/icons-png/*',
+                'static/js/jquery-mobile/images/icons-svg/*',
+                'templates/*',
+                'templates/security/*',
+                'templates/security/email/*',
+            ],
+        },
+    )
+    setup(**setup_args)
 
-setup(**setup_args)
+else:  # using setuptools : http://pythonhosted.org/setuptools/
+
+    setup(name='rostful',
+        version='0.0.7',
+        description='REST API for ROS',
+        url='http://github.com/asmodehn/rostful',
+        author='AlexV',
+        author_email='asmodehn@gmail.com',
+        license='BSD',
+        packages=['rostful'],
+        # this is better than using package data ( since behavior is a bit different from distutils... )
+        include_package_data=True,  # use MANIFEST.in during install.
+        install_requires=[
+            'Rostful-node',
+            'Flask',
+            'Flask-Celery-Helper',
+            'Flask-Cors',
+            'Flask-Script',
+            'Flask-Restful',
+        ],
+        zip_safe=False,  # TODO testing...
+    )
