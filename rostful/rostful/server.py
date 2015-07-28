4e4723da9d4a1e9aceced47d670abe6b3a26b53d
alexv <asmodehn@gmail.com>
2015-07-28 15:09:44 +0900

    passing a node name to rostful_node to avoid conflict with other instances of it.

--- rostful/rostful/server.py
+++ rostful/rostful/server.py
@@ -126,7 +126,7 @@ class Server(object):
 
          #One RostfulNode is needed for Flask.
          #TODO : check if still true with multiple web process
-         with rostful_node.RostfulCtx(argv=ros_args) as node_ctx:
+         with rostful_node.RostfulCtx(name='rostful', argv=ros_args) as node_ctx:
              self._setup(node_ctx.node, node_ctx.client)
 
              # Celery needs rostfulNode running, and uses it via python via an interprocess Pipe interface
