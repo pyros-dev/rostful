c034017fa61faa9ce97198170f872f1df91e5b63
alexv <asmodehn@gmail.com>
2016-03-04 10:21:22 +0900

    fixing install rules. cleanup migrations.

--- setup.py
+++ setup.py
@@ -23,10 +23,6 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
             'testfixtures',
             'flask_cors',
             'flask_restful', 'flask_restful.utils', 'flask_restful.representations',  # TODO ROSDEP has pip package
-            'flask_migrate',
-            'flask_security',
-            'flask_wtf', 'flask_wtf.recaptcha',
-            'flask_sqlalchemy',
             'passlib', 'passlib.ext', 'passlib.ext.django', 'passlib.handlers', 'passlib.tests', 'passlib.utils', 'passlib.utils._blowfish', 'passlib._setup',  # TODO : rosdep has a DEB package for this
             'click',  # TODO : use deb package http://packages.ubuntu.com/search?keywords=python-click-cli ROSDEP also has python-click from pip
             'webargs',
@@ -37,10 +33,6 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
             'testfixtures': 'deps/testfixtures/testfixtures',
             'flask_cors': 'deps/flask-cors/flask_cors',
             'flask_restful': 'deps/flask-restful/flask_restful',
-            'flask_migrate': 'deps/Flask-Migrate/flask_migrate',
-            'flask_security': 'deps/flask-security/flask_security',
-            'flask_wtf': 'deps/flask-wtf/flask_wtf',
-            'flask_sqlalchemy': 'deps/flask-sqlalchemy/flask_sqlalchemy',
             'passlib': 'deps/passlib/passlib',
             'click': 'deps/click/click',
             'webargs': 'deps/webargs/webargs',
@@ -49,8 +41,6 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
         # CATKIN doesn't work well with those => symlink workarounds
         py_modules=[
             'flask_login',
-            'flask_principal',
-            'flask_mail',
         ],
         package_data={
             'rostful': [
@@ -83,14 +73,11 @@ else:  # using setuptools : http://pythonhosted.org/setuptools/
         # this is better than using package data ( since behavior is a bit different from distutils... )
         include_package_data=True,  # use MANIFEST.in during install.
         install_requires=[
-            'rostful_node',
+            'pyros',
             'Flask==0.10.1',
-            'Flask-Celery-Helper',
             'Flask-Cors==2.0.1',
             'Flask-Script',
-            'Flask-Security==1.7.4',
             'Flask-Restful',
-            'Flask-SQLAlchemy==2.0',
             # python requests...
         ],
         zip_safe=False,  # TODO testing...
