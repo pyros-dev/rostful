ed1bfeb2c24a9984aec24b73be008b9da317425d
alexv <asmodehn@gmail.com>
2016-11-15 18:42:51 +0900

    removing passlib and rester from dependencies list. not needed now.

--- setup.py
+++ setup.py
@@ -22,31 +22,31 @@ if _CATKIN:  # using distutils : https://docs.python.org/2/distutils
             # If another package install the same pthon package dependency.
             # TODO : The proper solution is to create ThirdPartyRelease for these packages if needed or
             # TODO : Use existing ros package for these
-            'rester',
+            # 'rester',
             'testfixtures',
             'flask_cors',
             'flask_reverse_proxy',
             'flask_restful', 'flask_restful.utils', 'flask_restful.representations',  # TODO ROSDEP has pip package
-            'passlib', 'passlib.ext', 'passlib.ext.django', 'passlib.handlers', 'passlib.tests', 'passlib.utils', 'passlib.utils._blowfish', 'passlib._setup',  # TODO : rosdep has a DEB package for this
+            # 'passlib', 'passlib.ext', 'passlib.ext.django', 'passlib.handlers', 'passlib.tests', 'passlib.utils', 'passlib.utils._blowfish', 'passlib._setup',  # TODO : rosdep has a DEB package for this
             'click',  # TODO : use deb package http://packages.ubuntu.com/search?keywords=python-click-cli ROSDEP also has python-click from pip
             'webargs',
             'marshmallow',
         ],
         package_dir={
-            'rester': 'deps/Rester/rester',
+            # 'rester': 'deps/Rester/rester',
             'testfixtures': 'deps/testfixtures/testfixtures',
             'flask_cors': 'deps/flask-cors/flask_cors',
             'flask_reverse_proxy': 'deps/flask-reverse-proxy/flask_reverse_proxy',
             'flask_restful': 'deps/flask-restful/flask_restful',
-            'passlib': 'deps/passlib/passlib',
+            #'passlib': 'deps/passlib/passlib',
             'click': 'deps/click/click',
             'webargs': 'deps/webargs/webargs',
             'marshmallow': 'deps/marshmallow/marshmallow',
         },
         # CATKIN doesn't work well with those => symlink workarounds
-        py_modules=[
-            'flask_login',
-        ],
+        # py_modules=[
+        #     'flask_login',
+        # ],
         package_data={
             'rostful': [
                 'static/favicon.ico',
@@ -88,23 +88,23 @@ else:  # using setuptools : http://pythonhosted.org/setuptools/
             # This can create potential conflicts in install space,
             # If another package install the same pthon package dependency.
             # TODO : The proper solution is to create ThirdPartyRelease for these packages
-            'rester',
+            # 'rester',
             'testfixtures',
             'flask_cors',
             'flask_restful', 'flask_restful.utils', 'flask_restful.representations',  # TODO ROSDEP has pip package
             'flask_reverse_proxy',
-            'passlib', 'passlib.ext', 'passlib.ext.django', 'passlib.handlers', 'passlib.tests', 'passlib.utils', 'passlib.utils._blowfish', 'passlib._setup',  # TODO : rosdep has a DEB package for this
+            # 'passlib', 'passlib.ext', 'passlib.ext.django', 'passlib.handlers', 'passlib.tests', 'passlib.utils', 'passlib.utils._blowfish', 'passlib._setup',  # TODO : rosdep has a DEB package for this
             'click',  # TODO : use deb package http://packages.ubuntu.com/search?keywords=python-click-cli ROSDEP also has python-click from pip
             'webargs',
             'marshmallow',
         ],
         package_dir={
-            'rester': 'deps/Rester/rester',
+            # 'rester': 'deps/Rester/rester',
             'testfixtures': 'deps/testfixtures/testfixtures',
             'flask_cors': 'deps/flask-cors/flask_cors',
             'flask_reverse_proxy': 'deps/flask-reverse-proxy/flask_reverse_proxy',
             'flask_restful': 'deps/flask-restful/flask_restful',
-            'passlib': 'deps/passlib/passlib',
+            # 'passlib': 'deps/passlib/passlib',
             'click': 'deps/click/click',
             'webargs': 'deps/webargs/webargs',
             'marshmallow': 'deps/marshmallow/marshmallow',
@@ -120,10 +120,10 @@ else:  # using setuptools : http://pythonhosted.org/setuptools/
             'futures == 3.0.2',
             'Flask==0.10.1',
             'Flask-Cors==2.0.1',
-            'Flask-Script',
+            #'Flask-Script',
             'Flask-Restful',
             'Flask-reverse-proxy',
-            'Rester',
+            #'Rester',
             'click',
             'webargs',
             'pyros==0.1.0',
