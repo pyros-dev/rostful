532e0202ef4c582b156c676d34318b4c44ad7b65
AlexV <asmodehn@gmail.com>
2018-04-21 15:54:41 +0200

    integrated release commands to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,87 @@
-from setuptools import setup
+import setuptools
+import subprocess
 
 # using setuptools : http://pythonhosted.org/setuptools/
-# TODO : Use the bdist_wheel setuptools extension available from the wheel project to create wheels.
-# TODO : This as a third party package could be the ultimate goal...
 
 with open('rostful/_version.py') as vf:
     exec(vf.read())
+
+
+# Best Flow :
+# Clean previous build & dist
+# $ gitchangelog >CHANGELOG.rst
+# change version in code and changelog (AND in package .xml for ROS !)
+# $ python setup.py prepare_release
+# WAIT FOR TRAVIS CHECKS
+# $ python setup.py publish
+# => TODO : try to do a simpler "release" command
+
+#
+# And then for ROS, directly (tag is already in git repo):
+# bloom-release --rosdistro kinetic --track kinetic pyros_interfaces_ros
+
+
+# Clean way to add a custom "python setup.py <command>"
+# Ref setup.py command extension : https://blog.niteoweb.com/setuptools-run-custom-code-in-setup-py/
+class PrepareReleaseCommand(setuptools.Command):
+    """Command to release this package to Pypi"""
+    description = "prepare a release of pyros"
+    user_options = []
+
+    def initialize_options(self):
+        """init options"""
+        pass
+
+    def finalize_options(self):
+        """finalize options"""
+        pass
+
+    def run(self):
+        """runner"""
+
+        # TODO :
+        # $ gitchangelog >CHANGELOG.rst
+        # change version in code and changelog
+        subprocess.check_call("git commit CHANGELOG.rst rostful/_version.py -m 'v{0}'".format(__version__), shell=True)
+        subprocess.check_call("git push", shell=True)
+
+        print("You should verify travis checks, and you can publish this release with :")
+        print("  python setup.py publish")
+        sys.exit()
+
+
+# Clean way to add a custom "python setup.py <command>"
+# Ref setup.py command extension : https://blog.niteoweb.com/setuptools-run-custom-code-in-setup-py/
+class PublishCommand(setuptools.Command):
+    """Command to release this package to Pypi"""
+    description = "releases pyros to Pypi"
+    user_options = []
+
+    def initialize_options(self):
+        """init options"""
+        # TODO : register option
+        pass
+
+    def finalize_options(self):
+        """finalize options"""
+        pass
+
+    def run(self):
+        """runner"""
+        # TODO : clean build/ and dist/ before building...
+        subprocess.check_call("python setup.py sdist", shell=True)
+        subprocess.check_call("python setup.py bdist_wheel", shell=True)
+        # OLD way:
+        # os.system("python setup.py sdist bdist_wheel upload")
+        # NEW way:
+        # Ref: https://packaging.python.org/distributing/
+        subprocess.check_call("twine upload dist/*", shell=True)
+
+        subprocess.check_call("git tag -a {0} -m 'version {0}'".format(__version__), shell=True)
+        subprocess.check_call("git push --tags", shell=True)
+        sys.exit()
+
+
 
 # https://hynek.me/articles/conditional-python-dependencies/
 import sys
@@ -14,7 +90,7 @@ if sys.version_info[0:3] <= (2, 7, 9) or (3, 0) < sys.version_info[0:2] <= (3, 4
     sys_requires += ['backports.ssl-match-hostname']
 
 
-setup(
+setuptools.setup(
     name='rostful',
     version=__version__,
     description='REST API for ROS',
@@ -71,6 +147,10 @@ setup(
     tests_require=[
         'nose>=1.3.7'
     ],
+    cmdclass={
+        'prepare_release': PrepareReleaseCommand,
+        'publish': PublishCommand,
+    },
     zip_safe=False,  # TODO testing...
 )
 
